7d8da8ce97a93afc8ad8c567a47c45111076a549
Duvan G <duvan258@gmail.com>
2019-03-18 16:14:28 -0500

    FE - Star of contact form

--- frontend/src/components/Contact/index.js
+++ frontend/src/components/Contact/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { TextField } from 'react-md';
+import './styles.scss';
 
 const Contact = () => (
-  <div>
-    Contact
+  <div className="contact">
+    <div className="contact__form">
+      <TextField
+        id="floating-center-title"
+        label="First Name"
+        lineDirection="center"
+        placeholder="Your name..."
+        maxLength={255}
+        className="md-cell md-cell--bottom"
+      />
+    </div>
   </div>
 );
 
