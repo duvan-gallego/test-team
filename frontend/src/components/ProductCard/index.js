5dd658738ff1896085a8d487f741ce2a8284fb0e
Duvan G <duvan258@gmail.com>
2019-03-20 10:20:51 -0500

    FE - Add products styles in responsive

--- frontend/src/components/ProductCard/index.js
+++ frontend/src/components/ProductCard/index.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import { Card, CardTitle, CardText } from 'react-md';
+import cn from 'classnames';
+import { Card, CardTitle, CardText, Cell } from 'react-md';
 import './styles.scss';
 
-const ProductCard = ({ name, description, price, brand, stock, photo, categories  }) => (
-  <Card className="card">
-    <CardTitle title={name} subtitle={brand} />
-
-    <CardText className="card__content">      
-      <img className="card__image" src={photo} alt={name} />      
-      <p className="card__text">
-        {description}
-        stock: {stock}
-        price: {price}
-      </p>      
-    </CardText>
-
-  </Card>
+const ProductCard = ({ name, description, price, brand, stock, photo, categories, columnSize, twoColumnsSize }) => (
+  <Cell size={columnSize} tabletSize={12} phoneSize={12}>
+    <Card className={cn('productCard', { 'productCard--two-columns' : columnSize === twoColumnsSize })}>
+      <CardTitle title={name} subtitle={brand} />
+      <CardText className="productCard__content">
+        <img className="productCard__image" src={photo} alt={name} />
+        <div className="productCard__text">
+          <div className="productCard__text--description">
+            {description}
+          </div>
+          <div>
+            <b>Stock:</b> {stock}
+          </div>
+          <div>
+            <b>Price:</b> {price}
+          </div>
+        </div>
+      </CardText>
+    </Card>
+  </Cell>
 );
 
 export default ProductCard;
--- frontend/src/containers/ProductsList/index.js
+++ frontend/src/containers/ProductsList/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontIcon, TextField, Grid } from 'react-md';
+import cn from 'classnames';
 import ProductCard from '../../components/ProductCard';
 import './styles.scss';
 
+const FULL_COLUMN_SIZE = 12;
+const TWO_COLUMNS_SIZE = 6;
 
 const ProductsList = () => {
+
+  const [columnSize, setColumnSize] = useState(12);
 
   const list = [
     {
@@ -29,12 +35,74 @@ const ProductsList = () => {
       "categories": [
         "Office"
       ]
+    },
+    {
+      "id": "bde2ab21-8880-4552-8f3a-e575b0513a1d",
+      "name": "Ergonomic Rubber Chips",
+      "description": "Esse eum sapiente. Impedit accusantium officiis similique sit veritatis. Quo nam quia sapiente non quas qui.",
+      "price": "149.00",
+      "brand": "Murphy Inc",
+      "stock": 7793,
+      "photo": "http://lorempixel.com/640/480/technics",
+      "categories": ["Tech", "Office"]
+    }, {
+      "id": "a8946d49-2ed5-4e8d-87ce-8e9900dfdc22",
+      "name": "Gorgeous Steel Chips",
+      "description": "Eos rem assumenda. Minima aperiam pariatur deleniti mollitia sed. Maiores saepe magnam. Et voluptates omnis corrupti. Eos possimus reiciendis repudiandae ea voluptatum. Omnis corporis similique.",
+      "price": "268.00",
+      "brand": "Fadel, Lemke and Weber",
+      "stock": 98720,
+      "photo": "http://lorempixel.com/640/480/business",
+      "categories": ["Office"]
+    }, {
+      "id": "747a3b2c-8125-4dca-9c37-9e4b41b8a77a",
+      "name": "Refined Frozen Table",
+      "description": "Nulla occaecati qui laboriosam reprehenderit veritatis ea eaque. Blanditiis consequuntur enim mollitia reprehenderit est sunt corrupti quibusdam error. Provident minus non labore et alias aut quos rerum.",
+      "price": "526.00",
+      "brand": "Carter and Sons",
+      "stock": 28915,
+      "photo": "http://lorempixel.com/640/480/food",
+      "categories": ["Office"]
     }
   ];
 
+  const handlerViewModeClick = (size) => {
+    setColumnSize(size);
+  }
+
   return (
     <div className="productsList">
-      { list.map((item) => <ProductCard key={ item.id } { ...item } />) }
+      <header className="productsList__header">
+        <div className="productsList__rows_view">
+          <div>
+            <FontIcon
+              className={cn('productsList__icon', { 'productsList__icon--active' : columnSize === FULL_COLUMN_SIZE })}
+              onClick={() => handlerViewModeClick(FULL_COLUMN_SIZE)}
+            >
+              view_list
+            </FontIcon>
+            <FontIcon
+              className={cn('productsList__icon', { 'productsList__icon--active' : columnSize === TWO_COLUMNS_SIZE })}
+              onClick={() => handlerViewModeClick(TWO_COLUMNS_SIZE)}
+            >
+              view_module
+            </FontIcon>
+          </div>
+          <div>
+            Showing <b>10</b> products - Hidden: <b>5</b>
+          </div>
+        </div>
+        <TextField
+          id="search"
+          label="Search"
+          lineDirection="center"
+          placeholder="Type any word to search..."
+          className="md-cell md-cell--bottom productsList__search"
+        />
+      </header>
+      <Grid>
+        { list.map((item) => <ProductCard key={ item.id } columnSize={columnSize} { ...item } twoColumnsSize={TWO_COLUMNS_SIZE} />) }
+      </Grid>
     </div>
   )
 };
